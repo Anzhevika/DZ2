f20f1012a349d5902a4786e66cf0543c5c4ff79c
Angelikam <an_lev@yahoo.com>
2023-01-21 21:46:21 +0300

    Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("ВВедите пожалуйста число : ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+string num3 = Convert.ToString(num);
+
+if (num3.Length < 3)
+{
+  System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(num3[2]);
+}
+
+
